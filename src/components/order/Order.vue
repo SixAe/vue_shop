0a22cfadfe19a2f9af035693d47d72033b509912
ShiQi <wdkssq@163.com>
2020-10-06 22:11:26 +0800

    新添加

---
+++ src/components/order/Order.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片区域 -->
+    <el-card class="box-card">
+      <!-- 搜索框   style="margin-top: 15px"-->
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-input
+            placeholder="请输入内容"
+            v-model="querInfo.query"
+            class="input-with-select"
+            clearable
+            @clear="getOrderList"
+            @keyup.enter.native="getOrderList"
+          >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="getOrderList"
+            ></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="goAddpage">添加商品</el-button>
+        </el-col>
+      </el-row>
+      <!-- 数据区 -->
+
+      <!-- 分页 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="querInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="querInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      querInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10,
+      },
+      total: 0,
+      orderList: [],
+    }
+  },
+  created() {
+    this.getOrderList()
+  },
+  computed: {},
+  methods: {
+    getOrderList() {},
+
+    handleSizeChange(newSize) {
+      this.querInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    // 监听pagenumber 的变化
+    handleCurrentChange(newPage) {
+      this.querInfo.pagenum = newPage
+      this.getOrderList()
+    },
+  },
+}
+</script>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+/* 导入进度条 */
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 /* 导入css */
 import './assets/css/global.css'
@@ -19,18 +22,29 @@ import {
 } from './assets/js/dateFormat.js'
 /* 导入网络请求的axios */
 import axios from 'axios'
+import {
+  config
+} from 'vue/types/umd'
 /* 配置请求的根路径 */
 axios.defaults.baseURL = `http://127.0.0.1:8888/api/private/v1/`
 /* 拦截器 */
 axios.interceptors.request.use(
   function (config) {
     config.headers.Authorization = window.sessionStorage.getItem('token')
+    // 进度条开始
+    Nprogress.start()
     // 返回配置
     return config
     // 第二个函数是错误处理函数
   },
   function (err) {
     console.log(err)
+  }
+)
+axios.interceptors.response.use(
+  config => {
+    Nprogress.done()
+    return config
   }
 )
 /* 可用的方式一 */
@@ -63,7 +77,7 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 // 富文本挂载到VUE
-Vue.use(VueQuillEditor, /* { default global options } */)
+Vue.use(VueQuillEditor, /* { default global options } */ )
 new Vue({
   router,
   render: h => h(App)
