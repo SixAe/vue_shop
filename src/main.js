479d21b52cc36a4cf66c6de77b5b2d090a909c62
ShiQi <wdkssq@163.com>
2020-10-07 08:57:08 +0800

    修改报错问题

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
-/* 导入进度条 */
+
+/*导入进度条 */
 import Nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
 
@@ -22,17 +23,14 @@ import {
 } from './assets/js/dateFormat.js'
 /* 导入网络请求的axios */
 import axios from 'axios'
-import {
-  config
-} from 'vue/types/umd'
+
 /* 配置请求的根路径 */
 axios.defaults.baseURL = `http://127.0.0.1:8888/api/private/v1/`
 /* 拦截器 */
 axios.interceptors.request.use(
   function (config) {
-    config.headers.Authorization = window.sessionStorage.getItem('token')
-    // 进度条开始
     Nprogress.start()
+    config.headers.Authorization = window.sessionStorage.getItem('token')
     // 返回配置
     return config
     // 第二个函数是错误处理函数
